a045153a640368f52fdb9763f341ed90aecd1f23
Jesper Ladegaard <jesper@dax.dk>
2021-02-17 09:42:41 +0100

    initial commit

---
+++ OpenFTTH.Core.Tests/Util/LookupCollectionTests.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using Newtonsoft.Json;
+using OpenFTTH.Util;
+using System;
+using Xunit;
+
+namespace OpenFTTH.Core.Tests.Util.LookupCollection
+{
+    public class LookupCollectionTests
+    {
+        [Fact]
+        public void NewtonsoftJsonSerializationTest()
+        {
+            var foo1 = new Foo() { Id = Guid.NewGuid(), Name = "Hest" };
+            var foo2 = new Foo() { Id = Guid.NewGuid(), Name = "Hund" };
+
+            var lookupCollection = new LookupCollection<Foo>(new[] { foo1, foo2 });
+
+            var json = JsonConvert.SerializeObject(lookupCollection);
+
+            var deserializedObject = JsonConvert.DeserializeObject<LookupCollection<Foo>>(json);
+
+            deserializedObject[foo1.Id].Name.Should().Be("Hest");
+            deserializedObject[foo2.Id].Name.Should().Be("Hund");
+            deserializedObject.TryGetValue(foo1.Id, out var _).Should().BeTrue();
+            deserializedObject.TryGetValue(Guid.NewGuid(), out var _).Should().BeFalse();
+        }
+
+        private class Foo : IIdentifiedObject
+        {
+            public Guid Id { get; set; }
+
+            public string? Name { get; set; }
+
+            public string? Description { get; init; }
+        }
+
+
+    }
+}
---
+++ OpenFTTH.Core/IIdentifiedObject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace OpenFTTH.Core
+{
+    /// <summary>
+    /// This is a root class to provide common identification for all classes needing identification and naming attributes.
+    /// </summary>
+    public interface IIdentifiedObject
+    {
+        /// <summary>
+        /// IEC CIM: Master resource identifier (mRID) issued by a model authority. The mRID is unique within an exchange context. Global uniqueness is easily achieved by using a UUID, as specified in RFC 4122, for the mRID. The use of UUID is strongly recommended.
+        /// NB: We call this attribute Id and not mRID.
+        /// </summary>
+        public Guid Id { get; }
+
+        /// <summary>
+        /// IEC CIM: The name is any free human readable and possibly non unique text naming the object.
+        /// </summary>
+        public string? Name { get; }
+
+
+        /// <summary>
+        /// IEC CIM: The description is a free human readable text describing or naming the object. It may be non unique and may not correlate to a naming hierarchy.
+        /// </summary>
+        public string? Description { get; }
+    }
+}
